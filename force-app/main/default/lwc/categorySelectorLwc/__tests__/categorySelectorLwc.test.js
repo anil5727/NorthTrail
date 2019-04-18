1586a190dd9c5f2186db5278eeae2974c5673def
Paul <paulrobertaylor@gmail.com>
2019-04-18 14:20:02 +0100

    Create test for category selector and add picklist value mock data json

---
+++ force-app/main/default/lwc/categorySelectorLwc/__tests__/categorySelectorLwc.test.js
@@ -0,0 +1,44 @@
+import { createElement } from 'lwc';
+import categorySelectorLwc from 'c/categorySelectorLwc';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { CurrentPageReference } from 'lightning/navigation';
+import {
+    registerLdsTestWireAdapter,
+    registerTestWireAdapter
+} from '@salesforce/lwc-jest';
+
+const mockGetPicklistValues = require('./data/getPicklistValues.json');
+const mockGetObjectInfo = require('./data/getObjectInfo.json');
+
+const getPicklistValuesAdapter = registerLdsTestWireAdapter(getPicklistValues);
+const getObjectInfoAdapter = registerLdsTestWireAdapter(getObjectInfo);
+
+registerTestWireAdapter(CurrentPageReference);
+
+describe('c-category-selector-lwc', () => {
+    afterEach(() => {
+        // The jsdom instance is shared across test cases in a single file so reset the DOM
+        while (document.body.firstChild) {
+            document.body.removeChild(document.body.firstChild);
+        }
+    });
+
+    it('does not render', () => {
+        const element = createElement('c-category-selector-lwc', {
+            is: categorySelectorLwc
+        });
+        document.body.appendChild(element);
+
+        getObjectInfoAdapter.emit(mockGetObjectInfo);
+        getPicklistValuesAdapter.emit(mockGetPicklistValues);
+
+        return Promise.resolve().then(() => {
+
+            const combobox = element.shadowRoot.querySelector('lightning-combobox');
+            console.log('combobox');
+            console.log(JSON.stringify(combobox));
+        });
+
+    });
+});
