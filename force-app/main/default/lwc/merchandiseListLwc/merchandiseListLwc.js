9bd9168b19af6662b165f2948eba336b4b46074c
Paul <paulrobertaylor@gmail.com>
2019-04-18 12:45:51 +0100

    Handle category change and display result temporarily in the merchandise list.

--- force-app/main/default/lwc/merchandiseListLwc/merchandiseListLwc.js
+++ force-app/main/default/lwc/merchandiseListLwc/merchandiseListLwc.js
@@ -3,4 +3,9 @@ import { LightningElement, wire, track } from 'lwc';
 export default class MerchandiseListLwc extends LightningElement {
 
     @track total;
+    @track category;
+
+    categoryChangeHandler(event) {
+        this.category = event.detail.value;
+    }
 }
