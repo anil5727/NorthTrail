69f740394e496a2f7d9980fa1d5adb2b14c1b630
Paul <paulrobertaylor@gmail.com>
2019-04-18 13:00:24 +0100

    .

--- force-app/main/default/lwc/merchandiseListLwc/merchandiseListLwc.js
+++ force-app/main/default/lwc/merchandiseListLwc/merchandiseListLwc.js
@@ -1,9 +1,22 @@
 import { LightningElement, wire, track } from 'lwc';
+import getMerchandise from '@salesforce/apex/MerchandiseController.getMerchandise';
 
 export default class MerchandiseListLwc extends LightningElement {
 
     @track total;
     @track category;
+    @track merch;
+    @track error;
+
+    @wire(getMerchandise, {filters: '{"category":"", "searchKey":""}', pageSize: 5, pageNumber: 1})
+    wiredGetMerch({error, data}) {
+        if (data) {
+            this.merch = data.items;
+            this.total = data.total;
+        } else if (error) {
+            this.error = error;
+        }
+    }
 
     categoryChangeHandler(event) {
         this.category = event.detail.value;
